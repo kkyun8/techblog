c7e128cc0a46cedd395c3f902015ce82c07cb248
kkyun8 <kim23302@icloud.com>
2020-03-05 01:24:49 +0900

    add vuepress

---
+++ docs/.vuepress/dist/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: ["@vuepress/blog"],
+  title: "Checkout Skills",
+  themeConfig: {
+    description: "description",
+    nav: [
+      { text: "About", link: "/about/" } // ここはnav barのメニュー表示/aboutページは後ほど
+    ]
+  }
+};
